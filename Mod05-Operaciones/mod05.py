eed13f23d3f97e0146d7a4e38fbc581583c647db
Gabriela Esquivel <99062142+GabrielaEsquivel@users.noreply.github.com>
2022-02-12 22:36:57 -0600

    add mods 4 and 5

---
+++ Mod05-Operaciones/mod05.py
@@ -0,0 +1,57 @@
+# Suma
+answer = 30 + 12
+print(answer)
+
+# Resta
+difference = 30 - 12
+print(difference)
+
+
+# Multiplicación 
+product = 30 * 12
+print(product)
+
+#Divsión
+quotient = 30 / 12
+print(quotient)
+
+
+# Trabajando con la división
+seconds = 1042
+display_minutes = 1042 // 60
+display_seconds = 1042 % 60
+print(display_minutes)
+print(display_seconds)
+
+
+
+# Convirtiendo cadenas a núms
+demo_int = int('215')
+print(demo_int)
+
+demo_float = float('215.3')
+print(demo_float)
+
+
+
+# Valores absolutos 
+print(abs(39 - 16))
+print(abs(16 - 39))
+
+
+
+# Redondeo
+
+print(round(14.5))
+
+
+# Biblioteca Math
+# redondeos
+
+from math import ceil, floor
+
+round_up = ceil(12.5)
+print(round_up)
+
+round_down = floor(12.5)
+print(round_down)
